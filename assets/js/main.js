9308d9f71d4317ce6d5927e45c5726454d373809
Bharath <86903043+Bharath2006@users.noreply.github.com>
2024-06-08 16:08:41 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,7 +182,15 @@ function toggleTheme() {
 
   }
 }
-
+// Add event listener to each menu item to close the menu when clicked
+const menuItems = document.querySelectorAll('.nav-menu a');
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Close the menu
+    const menuBtn = document.getElementById("myNavMenu");
+    menuBtn.className = "nav-menu";
+  });
+});
 
 
 window.addEventListener('scroll', scrollActive)
